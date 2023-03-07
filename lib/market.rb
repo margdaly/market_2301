a0f1aac9dae3cf1045d565220c4b05e58b8453fd
margdaly <margaret.daly@gmail.com>
2023-03-07 11:09:40 -0700

    feat: Add #sorted_item_list to market

--- lib/market.rb
+++ lib/market.rb
@@ -24,4 +24,19 @@ class Market
       vendor.inventory.has_key?(item)
     end
   end
+
+  def sorted_item_list
+    sorted_item_list = []
+    @vendors.each do |vendor|
+      vendor.inventory.each do |item, _|
+        sorted_item_list << item.name
+      end
+    end
+    sorted_item_list.sort!
+    sorted_item_list.uniq.join(', ')
+  end
+
+  def total_inventory
+    #return a hash with items as keys and hash as values - this sub-hash should have two key/value pairs: quantity pointing to total inventory for that item and vendors pointing to an array of the vendors that sell that item.
+  end
 end
